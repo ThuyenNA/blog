206abea0ec85d498309d97c443cd8482055ecf49
An Thuyen <anthuyen0206@gmail.com>
2023-02-10 22:50:53 +0700

    cm router

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ const morgan = require("morgan");
 const hbs = require("express-handlebars");
 const path = require("path");
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 //http logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 //Template engine
 app.engine(
@@ -20,12 +28,8 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 console.log(path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+//route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
